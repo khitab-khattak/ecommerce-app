7e402deacbc619080183d5bc28f31d6813cd3e7d
Khitab-khattakgit commit -m first <khitabk29@gmail.com>
2025-09-25 15:12:53 +0500

    React route to create multiple pages and also convert the checkout page to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { HomePage } from "./pages/HomePage";
+import { Routes, Route } from "react-router";
 import "./App.css";
-
-
+import { CheckoutPage } from "./pages/CheckoutPage";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route index element={<HomePage/>}/>
+        <Route path="checkout" element={<CheckoutPage/>}/>
+        <Route/>
+      </Routes>
     </>
   );
 }
