f3e24e1ff551d0c5b5c597a7b0f43282d6e98a01
Khitab-khattakgit commit -m first <khitabk29@gmail.com>
2025-09-26 09:55:02 +0500

    convert order and tracking file and split header into component and also use Link to avoid page reloading

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { HomePage } from "./pages/HomePage";
 import { Routes, Route } from "react-router";
 import "./App.css";
 import { CheckoutPage } from "./pages/CheckoutPage";
+import { TrackingPage } from "./pages/TrackingPage";
+import { OrdersPage } from "./pages/OrdersPage";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route index element={<HomePage/>}/>
         <Route path="checkout" element={<CheckoutPage/>}/>
+        <Route path="tracking" element={<TrackingPage/>}/>
+        <Route path="orders" element={<OrdersPage/>}/>
         <Route/>
       </Routes>
     </>
