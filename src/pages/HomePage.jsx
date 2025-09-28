bef58002cf6c303dced7b4992d9222509d732c06
Khitab-khattakgit commit -m first <khitabk29@gmail.com>
2025-09-28 07:20:08 +0500

    fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,66 +1,75 @@
-
 import "./HomePage.css";
 import { Header } from "../components/Header";
-import { products } from "../../starting code/data/products"
+import { products } from "../../starting code/data/products";
 export function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
   return (
     <>
       <title>Home</title>
-     <Header/>
+      <Header />
 
       <div className="home-page">
         <div className="products-grid">
-          {products.map((products)=>{
-            return(
+          {products.map((products) => {
+            return (
               <div key={products.id} className="product-container">
-              <div className="product-image-container">
-                <img
-                  className="product-image"
-                  src={products.image}
-                />
+                <div className="product-image-container">
+                  <img className="product-image" src={products.image} />
+                </div>
+
+                <div className="product-name limit-text-to-2-lines">
+                  {products.name}
+                </div>
+
+                <div className="product-rating-container">
+                  <img
+                    className="product-rating-stars"
+                    src={`images/ratings/rating-${
+                      products.rating.stars * 10
+                    }.png`}
+                  />
+                  <div className="product-rating-count link-primary">
+                    {products.rating.count}
+                  </div>
+                </div>
+
+                <div className="product-price">
+                  $ {(products.priceCents / 100).toFixed(2)}
+                </div>
+
+                <div className="product-quantity-container">
+                  <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                  </select>
+                </div>
+
+                <div className="product-spacer"></div>
+
+                <div className="added-to-cart">
+                  <img src="images/icons/checkmark.png" />
+                  Added
+                </div>
+
+                <button className="add-to-cart-button button-primary">
+                  Add to Cart
+                </button>
               </div>
-  
-              <div className="product-name limit-text-to-2-lines">
-               {products.name}
-              </div>
-  
-              <div className="product-rating-container">
-                <img
-                  className="product-rating-stars"
-                  src={`images/ratings/rating-${products.rating.stars * 10}.png`}
-                />
-                <div className="product-rating-count link-primary">{ products.rating.count}</div>
-              </div>
-  
-              <div className="product-price">$ { (products.priceCents/100).toFixed(2) }</div>
-  
-              <div className="product-quantity-container">
-                <select>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
-                </select>
-              </div>
-  
-              <div className="product-spacer"></div>
-  
-              <div className="added-to-cart">
-                <img src="images/icons/checkmark.png" />
-                Added
-              </div>
-  
-              <button className="add-to-cart-button button-primary">
-                Add to Cart
-              </button>
-            </div>
-            )
+            );
           })}
         </div>
       </div>
