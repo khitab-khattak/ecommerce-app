b974c1809d632c14eb8c69b2e74b47ff0a8ed13f
Khitab-khattakgit commit -m first <khitabk29@gmail.com>
2025-09-30 12:32:28 +0500

    Generate the HTML for OrderPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,25 @@ import "./App.css";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { TrackingPage } from "./pages/TrackingPage";
 import { OrdersPage } from "./pages/OrdersPage";
-import { useEffect , useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [cart , setCart] = useState([]);
-  useEffect(()=>{
-    axios.get('/api/cart-items?expand=product').then((response)=>{
-      setCart(response.data)
-    })
-  },[])
- 
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    axios.get("/api/cart-items?expand=product").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
+
   return (
     <>
       <Routes>
-        <Route index element={<HomePage cart = {cart}/>}/>
-        <Route path="checkout" element={<CheckoutPage cart={cart}/>}/>
-        <Route path="tracking" element={<TrackingPage/>}/>
-        <Route path="orders" element={<OrdersPage/>}/>
-        <Route/>
+        <Route index element={<HomePage cart={cart} />} />
+        <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+        <Route path="tracking" element={<TrackingPage />} />
+        <Route path="orders" element={<OrdersPage cart={cart} />} />
+        <Route />
       </Routes>
     </>
   );
